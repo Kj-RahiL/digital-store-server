a93734b898b9935352d6ef3cde45cc84b3b8fcaa
Kj-RahiL <rahiilarham@gmail.com>
2023-10-19 00:38:44 +0600

    crud operation for create and read declare

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const port = process.env.port || 3000
+const port = process.env.port || 8000;
 
 // middleware
 app.use(cors())
-app.use(express.json)
+app.use(express.json())
 
-app.get('/', (req,res)=>{
+// digital-store 
+// 7uNlEgfvud5YIqOu
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://digital-store:7uNlEgfvud5YIqOu@cluster0.uma9m7n.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db('productDB').collection('product')
+
+    // create
+    app.post('/product', async(req,res)=>{
+      const user = req.body
+      const result = await productCollection.insertOne(user)
+      res.send(result)
+    })
+
+    // read
+    app.get('/product', async(req,res)=>{
+      const cursor = productCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('My Digital server is running')
-})
-app.listen(port, ()=>{
+  })
+  
+  app.listen(port, () => {
     console.log(`MY digital store server is running on port: ${port}`)
-})
+  })
