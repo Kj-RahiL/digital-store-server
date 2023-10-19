aff3ed1b9e3c16fe7fdc4ee85386859e81656aa8
Kj-RahiL <rahiilarham@gmail.com>
2023-10-19 21:42:20 +0600

    adding .env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+require('dotenv').config()
 const cors = require('cors');
 const port = process.env.port || 8000;
 
@@ -7,12 +8,9 @@ const port = process.env.port || 8000;
 app.use(cors())
 app.use(express.json())
 
-// digital-store 
-// 7uNlEgfvud5YIqOu
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://digital-store:7uNlEgfvud5YIqOu@cluster0.uma9m7n.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uma9m7n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -43,8 +41,6 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
-
-
 
     
     // Send a ping to confirm a successful connection
