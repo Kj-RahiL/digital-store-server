38fdcf251117f020e1096676d698bcb82f21f0b5
Kj-RahiL <rahiilarham@gmail.com>
2023-10-20 18:54:23 +0600

    updated operation

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uma9m7n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('productDB').collection('product')
 
@@ -41,10 +41,44 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
+    app.get('/details/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/updateProduct/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query)
+      res.send(result)
+    })
 
+    // update
+    app.put('/updateProduct/:id', async(req,res)=>{
+      const id = req.params.id
+      const product = req.body
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateProduct = {
+        $set: {
+          name: product.name, 
+          brandName: product.brandName, 
+          category: product.category, 
+          image: product.image, 
+          price: product.price, 
+          rating : product.rating,
+          details: product.details
+
+        },
+      }
+      console.log(updateProduct)
+      const result = await productCollection.updateOne(filter, updateProduct, options);
+      res.send(result)
+    })
     
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
